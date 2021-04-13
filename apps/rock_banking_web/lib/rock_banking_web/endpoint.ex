98b0a951cbd1acb51327c68ef3d6f644f65cd4e2
alexander-p30 <alexanderp30e@gmail.com>
2021-04-13 18:42:41 -0300

    chore: set up formatter config

--- apps/rock_banking_web/lib/rock_banking_web/endpoint.ex
+++ apps/rock_banking_web/lib/rock_banking_web/endpoint.ex
@@ -7,29 +7,27 @@ defmodule RockBankingWeb.Endpoint do
     signing_salt: "dBuRJEPU"
   ]
 
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/",
     from: :rock_banking_web,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
-  )
 
   if code_reloading? do
-    plug(Phoenix.CodeReloader)
-    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :rock_banking_web)
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :rock_banking_web
   end
 
-  plug(Plug.RequestId)
-  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug(Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-  )
 
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
-  plug(Plug.Session, @session_options)
-  plug(RockBankingWeb.Router)
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug RockBankingWeb.Router
 end
